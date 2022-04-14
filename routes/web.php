465e98b10614c05ccbf717a31e8dc7e97767e754
fatimaahmed <fatima.ahmet170@gmail.com>
2022-04-14 15:26:14 +0300

    this is my new commits

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
+
 <?php
 
+use App\Http\Controllers\MyController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +18,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// this is the default index page
+Route::get('/', [MyController::class, 'index'])->name('home');
+// this is the rout-controller-view
+Route::get('/Param/{$p}', [MyController::class, 'test'])->name('test');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
